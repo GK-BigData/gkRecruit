b06dc52ed717901104708ecd61acacfdacd20d9e
cattle <1842029069@qq.com>
2019-07-24 11:06:35 +0800

    Merge branch 'dev' of github.com:GK-BigData/gkRecruit into dev
    # Conflicts:
    #	app/main/views.py

--- app/admin/叶水华练习/操作Excel/demo.py
+++ app/admin/叶水华练习/操作Excel/demo.py
@@ -19,30 +19,26 @@ class ETL():
     def __init__(self):
 
         self.engine = create_engine('mysql+pymysql://root:gkbigdata123456@192.168.43.33:3306/recruit')
-        #self.engine_test = create_engine('mysql+pymysql://root:gkbigdata123456@10.50.10.16:3306/test')
-
-        # self.conn = pymysql.connect(
-        #     host='10.50.10.16',
-        #     port=3306,
-        #     user='root',
-        #     passwd='gkbigdata123456',
-        #     db='test',
-        #     charset='utf8',
-        #     use_unicode=True
-        # )
         self.now_time=datetime.now()
 
     '''
     连接mysql，获得数据，同时将其转为Dataframe
     '''
 
-    def tra(self,lines):
+    def C_replaces(self,lines):
         list2 = ['广州', '深圳', '珠海', '汕头', '佛山', '韶关', '湛江', '肇庆', '江门', '茂名', '惠州', '梅州', '汕尾', '河源', '阳江',
                  '清远', '东莞', '中山', '潮州', '揭阳', '云浮']
         if lines[:2] in list2:
             return lines[:2] + '市'
         else:
             return lines
+
+    def S_replaces(self,need):
+        if need == '黑龙江省':
+            return need[:3]
+        else:
+            return need[:2]
+
 
     # 得到所需要更改的字段，将其转换成英文
     def etl(self, record,fields, params, params_2):
@@ -74,30 +70,47 @@ class ETL():
                 all_length=length_df-len(df)
                 re[param+'个数为']=all_length
 
-            '''
+            #必须为数字
             if param == '填充空值':
-                #将无效值变成Nan
+                #将数字效值变成Nan
                 df[need]=pd.to_numeric(df[need], errors='coerce')
                 print(df[need])
+
                 if params_2=='填充0':
-                    pass
+                    df[need]=df[need].fillna(0,inplace=True)
+                    re['填充0']='成功'
 
                 elif params_2=='填充平均值':
                     print(params_2)
-                    fill_avg_value=df[need].fillna(df[need].mean(),inplace=True)
+                    df[need]=df[need].fillna(df[need].mean(),inplace=True)
+                    re['填充平均值']='成功'
 
                 elif params_2=='填充中位数':
                     print(params_2)
-                    fill_median_value=df[need].fillna(df[need].median(),inplace=True)
-                    print(fill_median_value)
-            '''
+                    df[need]=df[need].fillna(df[need].median(),inplace=True)
+                    re['填充中位数']='成功'
+
+            #成市的转换
             if param == '转换城市':
                 if need=='student_address':
-                    df[need] = df[need].map(self.tra)
+                    df[need] = df[need].map(self.C_replaces)
                     re['转换城市']='成功'
+                else:
+                    city='请选择户口所在地（地区名称）'
+                    re['转换成市']='失败/未选择需要的字段'
+                    return city
 
+            #做地图需要的字段['广东','广西','贵州']
+            if param=='转换省份':
+                if need=='source_provinces':
+                    df[need]=df[need].map(self.S_replaces)
+                    re['转换省份']='成功'
+
+            #出生日期
             if param=='时间格式转换':
-                
+                if need=='Data_of_birth':
+                    df[need]=pd.to_datetime(df[need],format='%Y-%m-%d')
+                    re['转换时间格式']='成功'
 
 
         self.engine.execute('insert into record(status,time,zsyear,filename) values(%s,%s,%s,%s)','xx',self.now_time,2018,'a.xmls')
